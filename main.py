b4d5c942e68d0ea5ed9bfb416e847ff0a515a3b1
Bogdan Bobuh <bogdanbobuh70@gmail.com>
2026-02-16 19:50:27 +0200

    Fix indentation and syntax error in main.py

--- main.py
+++ main.py
@@ -3,23 +3,19 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-
-API_TOKEN = "8083855809:AAEEWBl0R9dIKSDLJxw7GhR0ouLRABkLeB4"
+API_TOKEN = "8388385129:AAEjfKcq4bP09lO-YvVWYNfiq6KQK07R9n0"
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-
 @dp.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer("Привіт! Я твій перший бот!")
- ]0
+    await message.answer("Привіт. Я твій перший бот")
+
+var = 0
 
 async def main():
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
     asyncio.run(main())
-
-#TEST1234
